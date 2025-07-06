a5edf1650803b96a304a84f2bfd6c207f0183074
kvzhao <kevin.zhao.dev@gmail.com>
2025-07-06 15:49:49 +0800

    update: setup error in CICD

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ from setuptools import setup, find_packages
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
+requirements = [
+    "opencv-python>=4.5.0",
+    "numpy>=1.19.0",
+]
 
 setup(
     name="simple-video-player",
