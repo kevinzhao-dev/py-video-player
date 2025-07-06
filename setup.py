31cbe98aa62411029f02f67f5036a2f0f17d8c0f
kvzhao <kevin.zhao.dev@gmail.com>
2025-07-06 14:51:45 +0800

    update: enahcne CLI installation

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kevinzhao-dev/py-video-player",
-    packages=find_packages(),
+    py_modules=["pp"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: End Users/Desktop",
